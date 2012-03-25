4513e4702c1eef969300d9e563a58148f8b02d48
Your Name <admin@eatloco.com>
2012-03-24 23:11:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Landingpage::Application.config.secret_token = '0e53b22456ca5f1c7ec5f65d3d49c07ed28138f50493e15aaafdd50144eacaafeb81bd9a98c32c70fa6c4e03e9509a646a769aaa90bf6ea0eda9b9e47c758bb1'
